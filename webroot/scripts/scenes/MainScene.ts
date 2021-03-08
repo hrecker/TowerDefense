4d4b8d1ddaca23f6535a6a0da2ee0229e07c845b
hrecker <henryjrecker@gmail.com>
2021-03-07 20:58:43 -0600

    Create room from tilemap using Tiled

--- webroot/scripts/scenes/MainScene.ts
+++ webroot/scripts/scenes/MainScene.ts
@@ -19,6 +19,8 @@ export class MainScene extends Phaser.Scene {
         this.load.image("ship", "assets/sprites/ship.png");
         this.load.image("target", "assets/sprites/target.png");
         this.load.image("block", "assets/sprites/block.png");
+
+        this.load.tilemapTiledJSON("room1", "assets/rooms/room1.json");
     }
 
     addPhysicsImage(x: number, y: number, name: string) {
@@ -33,16 +35,25 @@ export class MainScene extends Phaser.Scene {
     create() {
         this.cameras.main.setBackgroundColor(backgroundColor);
 
+        // Walls
         let floor = this.add.tileSprite(304, 592, 608, 32, "block");
         let ceiling = this.add.tileSprite(304, 16, 608, 32, "block");
         let left = this.add.tileSprite(16, 304, 32, 544, "block");
         let right = this.add.tileSprite(592, 304, 32, 544, "block");
         let walls = this.physics.add.staticGroup([floor, ceiling, left, right]);
 
+        // Room tiles
+        const roomMap = this.make.tilemap({ key: "room1" });
+        const tileset = roomMap.addTilesetImage("OneBlock", "block");
+        let blockLayer = roomMap.createLayer(0, tileset, 32, 32);
+        blockLayer.setCollisionByProperty({ collides: true });
+
+        // Ship
         ship = this.addPhysicsImage(200, 200, "ship");
         ship.body.setMaxSpeed(shipMaxSpeed);
 
         this.physics.add.collider(walls, ship);
+        this.physics.add.collider(blockLayer, ship);
 
         targetSprite = this.add.image(-1000, -1000, "target");
         this.setTarget(400, 400);
